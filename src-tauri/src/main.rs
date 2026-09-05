aeff96121ff0b514f317519d9f5b6cc812c987a9
yang.changvu <yang.changvu@gmail.com>
2026-09-06 01:45:38 +0700

    fix-console-window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,5 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Vuget is a desktop widget; never attach a Windows console to it.
+#![windows_subsystem = "windows"]
 
 mod config;
 mod lunar;
